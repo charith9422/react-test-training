5ae07f7932f57dd5bfdbd4a52a6ea4831e3e9e5b
Charith Abeygunawardhana <charith9422@gmail.com>
2020-06-08 17:43:28 +0530

    Added first react test project

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Test from './Test';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Test></Test>
+      <Author authorname="Charith Abeygunawardhana - Virtusa"></Author>
     </div>
   );
 }
-
+class Author extends Component{
+  render(){
+    return(
+    <div>
+      <h1>{this.props.authorname}</h1>
+      <button className="btn btn-primary">Btn</button>
+    </div>
+    );
+  }
+}
 export default App;
---
+++ src/Test.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react'
+
+export default class Test extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            header: "Header from state!...",
+            content: "Content from state"
+          // constructor is better place to set state and binding event handler methods
+          // do not directly update state.use set state method 
+          // can use both state and props in same application   
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                {/* <h1>{this.state.header}</h1> */}
+                <Header headerProp={this.state.header}></Header>
+                <h2>{this.state.content}</h2>
+            </div>
+        )
+    }
+}
+
+class Header extends Component {
+    render() {
+        return (
+            <div>
+                <h1>{this.props.headerProp}</h1>    
+            </div>
+        )
+    }
+}
+
